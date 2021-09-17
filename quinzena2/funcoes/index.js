690ab170dc81b680f06f1e5e9b2019d84e7c1369
RickHardBR <rickhard@bol.com.br>
2021-09-16 22:18:41 -0300

    fiz parte do exercicio, preciso terminar o resto, vou enviar depois termino

---
+++ quinzena2/funcoes/index.js
@@ -0,0 +1,23 @@
+//Exercício de escrita de código
+//01) - Escreva as funções explicadas abaixo:
+    //a) A função não deve receber nenhum parâmetro e deve imprimir uma mensagem falando algumas informações sobre você, como: 
+/*
+    function sobreMim(){
+    console.log('    Eu sou Ricardo Ribeiro, tenho 49 anos, mora em Conceição dos Ouros - MG. \n    Me falaram que sou muito TIOZÃO para aprender programação, estou acreditando nisso :(\n    Estou tentando mas não consigo entender algumas coisas.')
+}
+    sobreMim()
+
+*/
+//02) - Agora escreva uma função que receba 4 parâmetros que correspondem às informações de uma pessoa:
+//o nome (string), a idade (number), a cidade (string) e uma profissão (string). Ela deve retornar uma
+//string que unifique todas as informações da pessoa em uma só mensagem com o template:
+
+    //Eu sou [NOME], tenho [IDADE] anos, moro em [ENDEREÇO] e sou [PROFISSÃO].
+
+function informacoesPessoas(){
+let nome = "Ricardo Ribeiro"
+let idade = 49
+let profissao = "TI"
+let cidade = "Conceição dos ouros"
+
+}

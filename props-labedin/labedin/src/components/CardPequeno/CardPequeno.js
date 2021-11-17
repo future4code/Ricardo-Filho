b0b0fbab000f8ed006e66e044ff7e7daa45d3a3f
RickHardBR <rickhard@bol.com.br>
2021-11-17 19:00:01 -0300

    Algumas mudanças feitas

--- props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
+++ props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -5,11 +5,15 @@ function CardPequeno(props) {
     return (
         <div className="small-container">
             <div className="imagens">
-                <img id="tamanho-arroba" src={props.imagem1} alt="imagem-arroba"/>
-                <img id="tamanho-zap" src={props.imagem2} alt="imagem-WhatsApp"/>
+                <img id="tamanho-arroba" src={props.imagem1} alt="imagem-arroba"/> 
+            </div>
+            <div className="texto-contatos">
+                <h4><a className="link-mail" href="mailto:rickhard@bol.com.br">{props.email1}</a></h4>
             </div>
             <div className="imagens">
-                <h4><a className="link-mail" href="mailto:rickhard@bol.com.br">{props.email1}</a></h4>
+                <img id="tamanho-zap" src={props.imagem2} alt="imagem-WhatsApp"/>
+            </div>
+            <div className="texto-contatos">
                 <h4>{props.email2}</h4>
             </div>
         </div>

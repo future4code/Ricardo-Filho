faffc2a8e2e40d37f8a716b9e4f8dc7025c0a329
RickHardBR <rickhard@bol.com.br>
2022-07-12 22:09:30 -0300

    Exercicios para resolução de problemas

---
+++ modulo07/bloco22/resolvendo-problemas/src/exercise3/index.ts
@@ -0,0 +1,17 @@
+export const replaceMatrixValue = (
+  matrix: number[][],
+  rowIndex: number,
+  columnIndex: number,
+  value: number
+): void => {
+  if (
+    matrix[rowIndex] === undefined ||
+    matrix[rowIndex][columnIndex] === undefined
+  ) {
+    throw new Error("Fora do intervalo da matriz");
+  }
+
+  matrix[rowIndex][columnIndex] = value;
+};
+
+console.log(replaceMatrixValue([[1, 2, 3],[4, 5, 6]], 1, 1, 1));

e167e83bd0d2964e4e5a7103d14332bc535dd12d
RickHardBR <rickhard@bol.com.br>
2022-07-14 00:19:17 -0300

    Complexidade de Algoritmos

---
+++ modulo07/bloco22/complexidade-de-algoritmos/src/exercicio2/index.ts
@@ -0,0 +1,25 @@
+export const func = (
+   source: string,
+   comparison: string
+ ): boolean => {
+   if (
+     comparison.length > source.length + 1 ||
+     comparison.length < source.length - 1
+   ) {
+     return false;
+   }
+   let commonCharQuantity = 0;
+ 
+   for (const char of comparison) {
+     if (source !== comparison) {
+       commonCharQuantity++;
+     }
+   }
+   return (
+     commonCharQuantity <= source.length + 1 &&
+     commonCharQuantity >= source.length - 1
+   );
+ };
+
+ console.log(func('abc', 'bc'));
+ 

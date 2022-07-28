8a50f9fa3633d2745f385998cccdbb687ade4ae4
RickHardBR <rickhard@bol.com.br>
2022-07-27 21:19:31 -0300

    Início do case 2 - Front - Bloco 23 - Tarô

---
+++ modulo07/bloco23/taro/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>
+            Apenas dando início ao projeto Tarô - 2º CASE - Bloco 23
+          </h1>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
